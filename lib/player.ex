1e1b0d5eff6a45600354d044710a18499a6207da
Kayke simao <kayke.py@gmail.com>
2025-04-11 09:37:50 -0300

    Refact: variables of module create

--- lib/player.ex
+++ lib/player.ex
@@ -1,16 +1,17 @@
-defmodule Exmon.Player do
-  required_keys = [:life,:name, :move_rnd, :move_avg, :move_heal]
+defmodule ExMon.Player do
+  @required_keys [:life, :move_avg, :move_heal, :move_rnd, :name]
+  @max_life 100
 
-  @enforce_keys required_keys
-  defstruct required_keys
+  @enforce_keys @required_keys
+  defstruct @required_keys
 
   def build(name, move_rnd, move_avg, move_heal) do
-    %Exmon.Player{
-      name: name,
-      move_rnd: move_rnd,
+    %ExMon.Player{
+      life: @max_life,
       move_avg: move_avg,
       move_heal: move_heal,
-      life: 100
+      move_rnd: move_rnd,
+      name: name
     }
   end
 end
