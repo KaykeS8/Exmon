236e94f4d29fa9e06f4b7ca29df778468c954675
Kayke simao <kayke.py@gmail.com>
2025-04-11 11:47:15 -0300

    show status of game

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,6 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
+  alias ExMon.Game.Status
 
   @computer_name "WALL-E"
 
@@ -11,5 +12,7 @@ defmodule ExMon do
     @computer_name
     |> create_player(:puch, :kick, :heal)
     |> Game.start(player)
+
+    Status.print_round_message
   end
 end
