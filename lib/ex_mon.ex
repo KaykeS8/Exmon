2ab3e24776881b08ad984a5a2b8bf8f6610c9bf4
Kayke simao <kayke.py@gmail.com>
2025-04-13 19:17:24 -0300

    Code format

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -13,7 +13,7 @@ defmodule ExMon do
     |> create_player(:puch, :kick, :heal)
     |> Game.start(player)
 
-    Status.print_round_message
+    Status.print_round_message()
   end
 
   def make_move(move) do
@@ -23,6 +23,7 @@ defmodule ExMon do
   end
 
   defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
+
   defp do_move({:ok, move}) do
     case move do
       :move_heal -> "Realiza cura"
