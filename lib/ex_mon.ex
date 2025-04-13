5d869053d43fc01c4e610c2600e1a3014ea2e3c2
Kayke simao <kayke.py@gmail.com>
2025-04-13 20:23:45 -0300

    List of names add to computer and print a message to each round

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -2,18 +2,18 @@ defmodule ExMon do
   alias ExMon.{Game, Player}
   alias ExMon.Game.{Status, Actions}
 
-  @computer_name "WALL-E"
+  @computer_names ["WALL-E", "ROBOT", "Ninax9"]
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
   end
 
   def start_game(player) do
-    @computer_name
+    Enum.random(@computer_names)
     |> create_player(:puch, :kick, :heal)
     |> Game.start(player)
 
-    Status.print_round_message()
+    Status.print_round_message(Game.info())
   end
 
   def make_move(move) do
@@ -29,5 +29,6 @@ defmodule ExMon do
       :move_heal -> "Realiza cura"
       move -> Actions.attack(move)
     end
+    Status.print_round_message(Game.info())
   end
 end
